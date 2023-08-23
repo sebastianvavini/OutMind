0cf2be05286b20515a50dc82d241e677a679b334
Sebastiao Vinícius <sebastianbavini@hotmail.com>
2023-08-23 15:41:32 -0300

    first commit

---
+++ app/src/main/java/com/zazensoftwares/outmind/NivelConscienciaActivity.kt
@@ -0,0 +1,11 @@
+package com.zazensoftwares.outmind
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NivelConscienciaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nivel_consciencia)
+    }
+}
---
+++ app/src/main/java/com/zazensoftwares/outmind/PerguntaBasica1Activity.kt
@@ -0,0 +1,27 @@
+package com.zazensoftwares.outmind
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.zazensoftwares.outmind.databinding.ActivityPerguntaBasica1Binding
+
+class PerguntaBasica1Activity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var binding: ActivityPerguntaBasica1Binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityPerguntaBasica1Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.buttonAvancarPergunta2.setOnClickListener(this)
+    }
+
+    override fun onClick(p: View) {
+        if(p.id==R.id.button_avancar_pergunta2){
+            avancar()
+        }
+    }
+
+    fun avancar(){
+
+    }
+}
